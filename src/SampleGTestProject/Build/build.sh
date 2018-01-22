2a8cc98c81ce8c1b6d3791e7ff639814c5274ffa
Krzysztof.Hofman <khofman250377@gmail.com>
2018-01-22 23:20:30 +0100

    1. Fixed typo in doc files 2. Changed references to GTest/GMock folders to use name starting from capital letter 3. Fixed handling build.sh script parameters for running tests

--- src/SampleGTestProject/Build/build.sh
+++ src/SampleGTestProject/Build/build.sh
@@ -8,10 +8,10 @@ make
 
 PARAMCOUNT=$#
 
-if [ $PARAMCOUNT -lt 2 ]; then
+if [ $PARAMCOUNT -eq 1 ]; then
   # run all test bult above
-  ./$1
-elif [[ $2 == *"--gtest_filter="* ]]; then
+  $1
+elif [[ $PARAMCOUNT -eq 2 && $2 == *"--gtest_filter="* ]]; then
   # run specified subset of tests
-  ./$1 $2
+  $1 $2
 fi
