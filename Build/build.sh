bfcbfba37e62fc6a621aeb245e7aef6fb634ffcd
gofer-k <khofman250377@gmail.com>
2018-03-15 15:49:30 +0100

    Removed commented lines from bash script

--- Build/build.sh
+++ Build/build.sh
@@ -2,17 +2,3 @@
 
 # pass command line arugments to python script for further processing
 python ./build.py "$@"
-
-# Generate makefile for test project
-# cmake $BUILD_PATH
-
-# # build the project in current folder
-# make
-
-# if [ -n $RUNTEST ] && [ -n $TEST_FILTER ]; then
-#   echo "Running test: ${TARGET} with filter="$TEST_FILTER
-#   $TARGET --gtest_filter="*"$TEST_FILTER"*"
-# elif [ -n $RUNTEST ] && [ -z $TEST_FILTER ]; then
-#   echo "Running test: ${TARGET}"
-#   $TARGET
-# fi
